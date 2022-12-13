6ddf735525c76c9400810e464245a5e8edd3dfe7
Anton Mikhailouski <Lobetcki@gmail.com>
2022-12-14 00:19:33 +0300

    HW8MethodsObject ДЗ 13.12

--- src/HW8MethodsObject/Book.java
+++ src/HW8MethodsObject/Book.java
@@ -1,27 +1,51 @@
 package HW8MethodsObject;
 
+import java.util.Objects;
+
 public class Book {
 // ОПИСАНИЕ КЛАССА
     private Autor autorName;
-    private Autor autorLastName;
     private String bookName;
     private int publishingYear;
 
 // КОНСТРУКТОР
-    public Book(Autor autorName, Autor autorLastName, String bookName, int publishingYear) {
+    public Book(Autor autorName, String bookName, int publishingYear) {
         this.autorName = autorName;
-        this.autorLastName = autorLastName;
         this.bookName = bookName;
         this.publishingYear = publishingYear;
     }
 
-// ГЕТЫ
-    public Autor getAutorName() {
-        return this.autorName;
+// Метод @Override toString
+    @Override
+    public String toString() {
+        return this.autorName + " - " + this.bookName + ", " + this.publishingYear;
     }
 
-    public Autor getAutorLastName() {
-        return this.autorLastName;
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Book book = (Book) o;
+//        return publishingYear == book.publishingYear && autorName.equals(book.autorName) && bookName.equals(book.bookName);
+//    }
+
+    //// Метод @Override equals
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Book c1 = (Book) other;
+        return bookName.equals(c1.bookName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(autorName, bookName, publishingYear);
+    }
+
+    // ГЕТЫ
+    public Autor getAutorName() {
+        return this.autorName;
     }
 
     public String getBookName() {
